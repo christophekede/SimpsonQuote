f1f5fab4cd431f7a5f86409f02b696f0d4742c74
christophekede <chris.kede@yahoo.fr>
2020-05-24 16:38:32 +0200

    :sparkles: add previous button in detail_activity

--- app/src/main/java/com/github/christophekede/simpsonquote/DetailActivity.java
+++ app/src/main/java/com/github/christophekede/simpsonquote/DetailActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +18,15 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                getApplicationContext().startActivity(intent);
+
+            }
+        });
         ImageView img = findViewById(R.id.imageDetail);
         TextView quotVw = findViewById(R.id.quote);
         TextView name = findViewById(R.id.name);
